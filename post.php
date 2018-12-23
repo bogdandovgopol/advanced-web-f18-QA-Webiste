7bfdb1458807cf2024e5df22fd9a05929ff344b6
Bogdan Dovgopol <bogdandovgopol@Macintosh.local>
2018-12-23 11:38:00 +1100

    post detail page added

---
+++ post.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 6472
+ * Date: 11/12/2018
+ * Time: 10:18 AM
+ */
+
+namespace App;
+
+use App\Helper\Template;
+use App\Repository\PostRepository;
+
+include "vendor/autoload.php";
+
+$slug = $_GET['slug'];
+
+$postClass = new PostRepository();
+$post = $postClass->getPostBySlug($slug);
+
+return new Template('detail', ['post' => $post]);
